33820f5ecd21ed08c40f09f37a7de9c6acfa57a3
Foly93 <luis.vollmers@web.de>
2024-02-15 15:14:09 +0100

    minor changes

---
+++ create_wooroux_env.sh
@@ -0,0 +1,12 @@
+
+mamba create --no-default-packages -n wooroux python=3.11 -y
+mamba install -n wooroux -c conda-forge pandas numpy jupyter -y 
+mamba install -n wooroux -c conda-forge matplotlib natsort scipy typing -y 
+mamba install -n wooroux -c conda-forge jupyter_contrib_nbextensions -y
+mamba install -n wooroux -c conda-forge jupyterlab -y
+mamba install -n wooroux -c conda-forge mdanalysis -y
+mamba install -n wooroux -c conda-forge pymbar=4.0.1 -y
+mamba install -n wooroux nb_conda_kernels -y
+mamba install -n wooroux -c conda-forge natsort -y
+mamba install -n wooroux -c nvidia cuda-nvcc -y
+mamba install -n wooroux -c conda-forge varname -y
